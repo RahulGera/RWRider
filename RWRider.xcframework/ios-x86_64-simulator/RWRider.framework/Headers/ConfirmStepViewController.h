38e1aa81dcbe981e5e478557f00937fcfadadbf2
Rahul Gera <rahulgera05607@gmail.com>
2022-04-13 00:40:27 +0530

    Added new UI Added the mechanism to present/dismiss the sdk

---
+++ RWRider.xcframework/ios-x86_64-simulator/RWRider.framework/Headers/ConfirmStepViewController.h
@@ -0,0 +1,45 @@
+//
+//  ConfirmStepViewController.h
+//  RideAustin
+//
+//  Created by bpfullmetal on 3/9/22.
+//  Copyright © 2022 RideAustin.com. All rights reserved.
+//
+
+#ifndef ConfirmStepViewController_h
+#define ConfirmStepViewController_h
+
+#import "BaseViewController.h"
+#import "FlatButton.h"
+#import "NSString+Utils.h"
+
+@class ConfirmStepViewController;
+@protocol ConfirmStepViewControllerDelegate
+
+- (void)confirmStepViewControllerDidPhoneConfirmed;
+
+@end
+
+@class PaddedTextField;
+
+@interface ConfirmStepViewController : BaseViewController<UITextFieldDelegate>
+
+@property (nonatomic, weak) id<ConfirmStepViewControllerDelegate> delegate;
+
+@property (strong, nonatomic) IBOutlet UIView *topStepProgressBar;
+@property(nonatomic) CAGradientLayer *codeTextFieldGradientLayer;
+@property(nonatomic, strong) IBOutlet PaddedTextField* codeTextField;
+@property (weak, nonatomic) IBOutlet FlatButton *nextButton;
+@property (weak, nonatomic) IBOutlet FlatButton *resendButton;
+
+- (id)initWithPhone:(NSString*)phone token:(NSString*)token;
+- (IBAction)goBackScreen:(id)sender;
+- (IBAction)goBackHomeScreen:(id)sender;
+- (IBAction)textFieldEditingChanged:(UITextField *)sender;
+- (IBAction)resendCode:(id)sender;
+- (IBAction)submitNext:(UIButton *)sender;
+
+@end
+
+
+#endif /* ConfirmStepViewController_h */
---
+++ RWRider.xcframework/ios-x86_64-simulator/RWRider.framework/Headers/TermsStepViewController.h
@@ -0,0 +1,48 @@
+//
+//  TermsStepViewController.h
+//  RideAustin
+//
+//  Created by bpfullmetal on 3/9/22.
+//  Copyright © 2022 RideAustin.com. All rights reserved.
+//
+
+#ifndef TermsStepViewController_h
+#define TermsStepViewController_h
+
+#import "BaseViewController.h"
+#import "FlatButton.h"
+#import <WebKit/WebKit.h>
+
+@class TermsStepViewController;
+@protocol TermsStepViewControllerDelegate
+
+- (void)termsStepViewControllerDidTermsAccepted;
+
+@end
+@class PaddedTextField;
+
+@interface TermsStepViewController : BaseViewController
+
+@property (nonatomic, weak) id<TermsStepViewControllerDelegate> delegate;
+
+@property (strong, nonatomic) IBOutlet UIView *topStepProgressBar;
+@property (strong, nonatomic) IBOutlet WKWebView *termsWebView;
+@property (weak, nonatomic) IBOutlet FlatButton *acceptButton;
+@property (weak, nonatomic) IBOutlet FlatButton *viewTermsButton;
+@property (weak, nonatomic) IBOutlet FlatButton *agreeCheckButton;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *acceptBtnBottomConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *termsWebViewHeightConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *viewTermsBtnCenterYConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *agreeCheckBtnCenterYConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *viewTermsSpaceVerticalConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *agreeCheckSpaceVerticalConstraint;
+
+- (IBAction)goBackScreen:(id)sender;
+- (IBAction)goBackHomeScreen:(id)sender;
+- (IBAction)viewTermsWebView:(id)sender;
+- (IBAction)checkAgreeTerms:(id)sender;
+- (IBAction)submitAccept:(id)sender;
+
+@end
+
+#endif /* TermsStepViewController_h */
